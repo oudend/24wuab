59cd6491ba80feed2e45c81cf0d3c76226b9ef4a
oudend <ternermartin@gmail.com>
2024-02-06 10:09:28 +0100

    semi transparent mobile menu
    made mobile menu transparent and disabled scrolling for it

--- js/components/essentials/menu.js
+++ js/components/essentials/menu.js
@@ -6,8 +6,12 @@ console.log(menuButton);
 
 menuButton.addEventListener("click", () => {
   menuPopup.classList.add("menu-visible");
+  document.body.style.overflow = "hidden";
+  document.documentElement.style.overflow = "hidden";
 });
 
 menuExit.addEventListener("click", () => {
   menuPopup.classList.remove("menu-visible");
+  document.body.style.overflow = "visible";
+  document.documentElement.style.overflow = "visible";
 });
