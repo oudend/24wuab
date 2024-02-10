311779d538cdd8d36df99023237cbcc7fb364ccb
oudend <ternermartin@gmail.com>
2024-02-10 19:07:21 +0100

    display restructure. project page added
    Displays now have proper links to the renders and an early draft of the project page has been added.

--- js/components/essentials/menu.js
+++ js/components/essentials/menu.js
@@ -2,8 +2,6 @@ const menuButton = document.getElementById("menu-button");
 const menuPopup = document.getElementById("menu-popup");
 const menuExit = document.getElementById("menu-exit-button");
 
-console.log(menuButton);
-
 menuButton.addEventListener("click", () => {
   menuPopup.classList.add("menu-visible");
   document.body.style.overflow = "hidden";
